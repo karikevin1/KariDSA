6607421e91ccb9b3ccd051d50fbd77d86ac1e394
kevin lim <kevin lim@DESKTOP-4K0GH1D>
2017-12-19 18:40:33 +0800

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	src/ModuleA/AffiliatesRegistration.java
    	src/ModuleA/AffiliatesRegistrationADT.java

---
+++ src/ModuleB/ComboBoxObj.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModuleB;
+
+/**
+ *
+ * @author kevin lim
+ */
+public class ComboBoxObj {
+    private String indexName;
+    private int value;
+
+    public ComboBoxObj(String indexName, int value) {
+        this.indexName = indexName;
+        this.value = value;
+    }
+
+    public String getIndexName() {
+        return indexName;
+    }
+
+    public void setIndexName(String indexName) {
+        this.indexName = indexName;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    
+}
